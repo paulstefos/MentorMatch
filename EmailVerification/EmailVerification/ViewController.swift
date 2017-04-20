7abf40110d54f7216f0cc906057014d3a8e62349
ronishawhite <ronishawhite@gmail.com>
2017-04-20 16:33:58 -0400

    Add files via upload

---
+++ EmailVerification/EmailVerification/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  EmailVerification
+//
+//  Created by ronnie on 3/23/17.
+//  Copyright © 2017 ronnie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var confirmedPasswordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func signupButtonTapped(_ sender: Any) {
+        
+        let providedEmailAddress = emailTextField.text
+        let isEmailAddressValid = isValidEmailAddress(emailAddressString: providedEmailAddress!)
+        
+        
+        if isEmailAddressValid {
+            print("Email Address is valid.")
+            
+    }
+        else {
+            print("Email Adress is not valid.")
+            displayAlertMessage(messageToDisplay:"Email Adress is not valid." )
+        }
+        
+    }
+    
+    
+    
+    func isValidEmailAddress(emailAddressString: String) -> Bool {
+        
+        var returnValue = true
+        let emailRegEx = "[A-Z0-9a-z.-_]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}"
+        
+        do {
+            let regex = try NSRegularExpression(pattern: emailRegEx)
+            let nsString = emailAddressString as NSString
+            let results = regex.matches(in: emailAddressString, range: NSRange(location: 0, length: nsString.length))
+            
+            if results.count == 0
+            {
+                returnValue = false
+            }
+            
+        } catch let error as NSError {
+            print("invalid regex: \(error.localizedDescription)")
+            returnValue = false
+        }
+        
+        return  returnValue
+    }
+    
+    
+    func displayAlertMessage (messageToDisplay: String)
+    {
+        let alertController = UIAlertController(title: "Alert", message: messageToDisplay, preferredStyle: .alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+            
+            // Code in this block will trigger when OK button tapped.
+            print("Ok button tapped");
+            
+        }
+        
+        alertController.addAction(OKAction)
+        
+        self.present(alertController, animated: true, completion:nil)
+
+}
+}
